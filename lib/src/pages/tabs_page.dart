7deca41ae1804e30b5f77facc01601c0cb547615
Luis Rangel <luisrangelc@gmail.com>
2020-04-12 13:35:47 -0600

    first commit

---
+++ lib/src/pages/tabs_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class TabsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _Paginas(),
+      bottomNavigationBar: _Navegacion(),
+    );
+  }
+}
+
+class _Navegacion extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: 0,
+      items: [
+        BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline), title: Text('Para Ti')),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.public), title: Text('Encabezados'))
+      ],
+    );
+  }
+}
+
+class _Paginas extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      physics: NeverScrollableScrollPhysics(),
+      children: <Widget>[
+        Container(
+          color: Colors.red,
+        ),
+        Container(
+          color: Colors.green,
+        )
+      ],
+    );
+  }
+}
